c49d528441bf730f085dfe7625fbcc9acda0a9cb
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-06-08 12:44:04 +0530

    add

---
+++ Day_1/example.cpp
@@ -0,0 +1,34 @@
+// C++ program to demonstrate the execution of constructor
+// and destructor
+
+#include <iostream>
+  using namespace std;
+
+ 
+class Test
+{
+
+public:
+// User-Defined Constructor
+	Test ()
+  {
+	cout << "\n Constructor executed";
+  }
+  
+ 
+// User-Defined Destructor
+	~Test ()
+  {
+	cout << "\nDestructor executed";
+  }
+
+};
+
+int main () 
+ { 
+Test t;
+  
+ 
+return 0;
+
+}
