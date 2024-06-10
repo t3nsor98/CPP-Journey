83543fb1c2f1563279bec222f2b1c2cd4a151bdd
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-06-10 23:25:09 +0530

    yo

---
+++ Day_2/operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter your first number" << endl;
+    cin >> a;
+    cout << "Enter your second number" << endl;
+    cin >> b;
+
+    cout << "a + b is equal to " << a + b;
+
+    return 0;
+}
---
+++ Day_3/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string name = "Hello World";
+    cout<<name<<endl;
+    cout<<"Length of name is "<<name.length()<<endl;
+    return 0;
+}
