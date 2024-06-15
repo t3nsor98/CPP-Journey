c68e2f045e2caba7df7b7c796cf2ee467d473004
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-06-15 19:13:37 +0530

    Day 5

---
+++ Day_5/yoyo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name;
+    int roll_no,age, standard;
+    cin>>name>>roll_no>>age>>standard;
+    cout<<name<<endl<<roll_no<<endl<<age<<endl<<standard<<endl;
+    return 0;
+}
