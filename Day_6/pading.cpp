d756e139f10079d9be2913e4572ebe388c6a0c95
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-06-17 18:04:55 +0530

    Day 7

---
+++ Day_6/pading.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class a {
+    char d;
+    // int a;
+    double e;
+    char b;
+
+};
+
+int main() {
+
+    cout << "Size of a is : " << sizeof(a) << endl;
+
+    return 0;
+
+}
